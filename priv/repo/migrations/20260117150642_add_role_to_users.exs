96b36326d31c33d377dd04809de0b35b46065cd1
amarantec <marcoscarvalhop@outlook.com>
2026-01-18 13:48:35 -0300

    add role for user middleware

---
+++ priv/repo/migrations/20260117150642_add_role_to_users.exs
@@ -0,0 +1,9 @@
+defmodule Api.Repo.Migrations.AddRoleToUsers do
+  use Ecto.Migration
+
+  def change do
+		alter table(:users) do
+			add :role, :string, default: "customer", nil: :false
+		end
+  end
+end
