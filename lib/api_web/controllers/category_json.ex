b3055d2193a62d03c516fa811270a225ac6bcf02
amarantec <marcoscarvalhop@outlook.com>
2026-01-18 13:47:10 -0300

    controller for category module

---
+++ lib/api_web/controllers/category_json.ex
@@ -0,0 +1,24 @@
+defmodule ApiWeb.CategoryJSON do
+  alias Api.Categories.Category
+
+  @doc """
+  Renders a list of categories.
+  """
+  def index(%{categories: categories}) do
+    %{data: for(category <- categories, do: data(category))}
+  end
+
+  @doc """
+  Renders a single category.
+  """
+  def show(%{category: category}) do
+    %{data: data(category)}
+  end
+
+  defp data(%Category{} = category) do
+    %{
+      id: category.id,
+      name: category.name
+    }
+  end
+end
